b42855713b6410f8bd5aa1b0d35103af2b7745ae
Malik Saafir <msaafir99@gmail.com>
2021-03-26 18:41:16 -0400

    Print target shape

--- main.py
+++ main.py
@@ -428,6 +428,7 @@ def train(train_loader, model, criterion, optimizer, epoch):
         # measure data loading time
         data_time.update(time.time() - end)
 
+        print(f"target shape is {target.shape}")
         target = target.cuda()
         input_var = torch.autograd.Variable(input)
         target_var = torch.autograd.Variable(target)

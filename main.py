ebb8ccf836ebf1b9b616cf085729677fd9a01be6
Malik Saafir <msaafir99@gmail.com>
2021-03-26 18:36:41 -0400

    Add print statement

--- main.py
+++ main.py
@@ -566,6 +566,7 @@ def accuracy(output, target, topk=(1,)):
     maxk = max(topk)
     batch_size = target.size(0)
 
+    print(f"{output=}")
     _, pred = output.topk(maxk, 1, True, True)
     pred = pred.t()
     correct = pred.eq(target.view(1, -1).expand_as(pred))
